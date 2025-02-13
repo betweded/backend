e77efc04e882324afc15d9abac867efcf978b61b
hemensol <hemensol14@yahoo.com>
2025-02-13 11:37:21 -0800

    phase 2 complete

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::API
+    before_action :set_active_storage_url_options
+
+    private
+  
+    def set_active_storage_url_options
+      ActiveStorage::Current.url_options = { host: request.base_url }
+    end
 end
